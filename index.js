9b69092aabaca727a28fed90a75024d30e15fe01
ayakub <ayakub1086@gmail.com>
2022-11-25 19:27:37 +0600

    added data base add product

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 
 // middleware
 app.use(cors())
 app.use(express.json())
-const password = process.env.CK_PASSWORD
-console.log(password)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@sickcluster.nqy80.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const catagoriesCollection = client.db('reusedCar').collection('catagories');
+        app.post('/catagory', async (req, res) => {
+            const catagory = req.body;
+            const result = await catagoriesCollection.insertOne(catagory);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('server is runnning')
 })
 app.listen(port, () => {
